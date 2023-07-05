bec3ae2a799d39f25d1f295cf92ea542aeab68e3
kstandbridge <prepotency@gmail.com>
2023-07-05 18:13:55 +0100

    chore: all warnings

--- hello_test/build.sh
+++ hello_test/build.sh
@@ -4,7 +4,7 @@ CurDir=$(pwd)
 BuildDir="$CurDir/bin"
 
 IncludeDirectories="-I $CurDir/kengine"
-CommonCompilerFlags="-Og -g -ggdb -msse4.2 -maes $IncludeDirectories"
+CommonCompilerFlags="-Og -g -ggdb -msse4.2 -maes -Wall -Wno-unused-function -Wno-missing-braces $IncludeDirectories"
 CommonLinkerFlags="-pthread"
 InternalCompilerFlags="-DKENGINE_LINUX -DKENGINE_INTERNAL -DKENGINE_SLOW"
 
