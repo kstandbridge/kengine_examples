577b4ca5e240980b52724b53d9405d4addc6cbf8
Kenneth Standbridge <kenneth.standbridge@ubisoft.com>
2023-07-20 08:56:13 +0100

    feat: win32 haversine processor test

---
+++ computer_enhance/haversine_processor/build.bat
@@ -0,0 +1,19 @@
+@echo off
+
+SET CurDir=%cd%
+SET BuildDir=%CurDir%\bin
+
+set IncludeDirectories=-I%CurDir%\kengine
+set CommonCompilerFlags=-Od -FC -Z7 -nologo %IncludeDirectories%
+set CommonLinkerFlags=-incremental:no -opt:ref
+set InternalCompilerFlags=-DKENGINE_WIN32 -DKENGINE_INTERNAL -DKENGINE_SLOW
+
+IF NOT EXIST %BuildDir% mkdir %BuildDir%
+
+pushd %BuildDir%
+
+cl %CommonCompilerFlags% %InternalCompilerFlags% %CurDir%\computer_enhance\haversine_processor\haversine_test.c /link %CommonLinkerFlags%
+haversine_test.exe
+
+popd
+
