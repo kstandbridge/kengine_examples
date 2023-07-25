be9b3b35aad964405f0f478522881a43f9af1ae8
kstandbridge <prepotency@gmail.com>
2023-07-25 09:55:34 +0100

    feat: CPU timer

--- computer_enhance/haversine_processor/performance_metrics.c
+++ computer_enhance/haversine_processor/performance_metrics.c
@@ -13,8 +13,9 @@ MainLoop(app_memory *AppMemory)
     //app_state *AppState = AppMemory->AppState = BootstrapPushStruct(app_state, Arena);   
 
     u64 OSFrequency = PlatformGetOSTimerFrequency();
-    PlatformConsoleOut("\tOS Frequency: %lu\n", OSFrequency);
+    PlatformConsoleOut("\t\tOS Frequency: %lu\n", OSFrequency);
 
+    u64 CPUStart = PlatformReadCPUTimer();
     u64 OSStart = PlatformReadOSTimer();
     u64 OSEnd = 0;
     u64 OSElapsed = 0;
@@ -24,8 +25,13 @@ MainLoop(app_memory *AppMemory)
         OSElapsed = OSEnd - OSStart;
     }
 
-    PlatformConsoleOut("\tOS Timer: %lu -> %lu = %lu elapsed\n", OSStart, OSEnd, OSElapsed);
+    u64 CPUEnd = PlatformReadCPUTimer();
+    u64 CPUElapsed = CPUEnd - CPUStart;
+
+    PlatformConsoleOut("\t\tOS Timer: %lu -> %lu = %lu elapsed\n", OSStart, OSEnd, OSElapsed);
     PlatformConsoleOut("\tOS Seconds: %.4f\n", (f64)OSElapsed/(f64)OSFrequency);
+
+    PlatformConsoleOut("\t\tCPU Timer: %lu -> %lu = %lu elapsed\n", CPUStart, CPUEnd, CPUElapsed);
 
     return 0;
 }
