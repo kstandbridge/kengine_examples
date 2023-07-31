9ab777566e54b1caef29137e03386bd221e96c23
Kenneth Standbridge <kenneth.standbridge@ubisoft.com>
2023-07-31 14:53:38 +0100

    chore: larger sample size

--- computer_enhance/haversine_processor/build.bat
+++ computer_enhance/haversine_processor/build.bat
@@ -4,7 +4,7 @@ SET CurDir=%cd%
 SET BuildDir=%CurDir%\bin
 
 set IncludeDirectories=-I%CurDir%\kengine
-set CommonCompilerFlags=-Od -FC -Z7 -nologo %IncludeDirectories%
+set CommonCompilerFlags=-O2 -FC -Z7 -nologo %IncludeDirectories%
 set CommonLinkerFlags=-incremental:no -opt:ref
 set InternalCompilerFlags=-DKENGINE_WIN32 -DKENGINE_INTERNAL -DKENGINE_SLOW
 
@@ -12,14 +12,14 @@ IF NOT EXIST %BuildDir% mkdir %BuildDir%
 
 pushd %BuildDir%
 
-REM cl %CommonCompilerFlags% %InternalCompilerFlags% %CurDir%\computer_enhance\haversine_processor\haversine_test.c /link %CommonLinkerFlags%
+REM cl %CommonCompilerFlags% %InternalCompilerFlags%\n %CurDir%\computer_enhance\haversine_processor\haversine_test.c /link %CommonLinkerFlags%
 REM haversine_test.exe
 
 REM cl %CommonCompilerFlags% %InternalCompilerFlags% %CurDir%\computer_enhance\haversine_processor\haversine_generator.c /link %CommonLinkerFlags%
-REM haversine_generator.exe uniform 1127504674 1000000
+REM haversine_generator.exe uniform 1127504674 10000000
 
 cl %CommonCompilerFlags% %InternalCompilerFlags% %CurDir%\computer_enhance\haversine_processor\haversine_processor.c /link %CommonLinkerFlags%
-haversine_processor.exe output.json output.f64
+haversine_processor.exe output.json
 
 REM cl %CommonCompilerFlags% %InternalCompilerFlags% %CurDir%\computer_enhance\haversine_processor\performance_metrics.c /link %CommonLinkerFlags%
 REM performance_metrics.exe 1000
