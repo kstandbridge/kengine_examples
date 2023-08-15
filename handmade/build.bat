4c43bb711aa5a4be298a37334d2fc6c9af8cadfe
kstandbridge <prepotency@gmail.com>
2023-08-15 10:43:49 +0100

    002 - Opening a Win32 Window

--- handmade/build.bat
+++ handmade/build.bat
@@ -5,7 +5,7 @@ SET BuildDir=%CurDir%\bin
 
 set IncludeDirectories=-I%CurDir%\kengine
 set CommonCompilerFlags=-Od -FC -Z7 -nologo %IncludeDirectories%
-set CommonLinkerFlags=-incremental:no -opt:ref user32.lib
+set CommonLinkerFlags=-incremental:no -opt:ref user32.lib gdi32.lib
 set InternalCompilerFlags=-DKENGINE_WIN32 -DKENGINE_INTERNAL -DKENGINE_SLOW
 
 IF NOT EXIST %BuildDir% mkdir %BuildDir%
