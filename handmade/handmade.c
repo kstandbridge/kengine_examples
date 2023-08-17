19f11792a2b8e288ab5c84114058b080c5709dda
kstandbridge <prepotency@gmail.com>
2023-08-17 18:13:53 +0100

    feat: Platform-independent Sound Output

--- handmade/handmade.c
+++ handmade/handmade.c
@@ -1,4 +1,25 @@
 internal void
+GameOutputSound(sound_output_buffer *SoundBuffer, s32 ToneHz)
+{
+    local_persist f32 tSine;
+    s16 ToneVolume = 3000;
+    s32 WavePeriod = SoundBuffer->SamplesPerSecond/ToneHz;
+
+    s16 *SampleOut = SoundBuffer->Samples;
+    for(int SampleIndex = 0;
+        SampleIndex < SoundBuffer->SampleCount;
+        ++SampleIndex)
+    {
+        f32 SineValue = sinf(tSine);
+        s16 SampleValue = (s16)(SineValue * ToneVolume);
+        *SampleOut++ = SampleValue;
+        *SampleOut++ = SampleValue;
+
+        tSine += 2.0f*Pi32*1.0f/(f32)WavePeriod;
+    }
+}
+
+internal void
 RenderWeirdGradient(offscreen_buffer *Buffer, s32 BlueOffset, s32 GreenOffset)
 {
     u8 *Row = (u8 *)Buffer->Memory;    
@@ -22,7 +43,9 @@ RenderWeirdGradient(offscreen_buffer *Buffer, s32 BlueOffset, s32 GreenOffset)
 }
 
 internal void
-GameUpdateAndRender(offscreen_buffer *Buffer, int BlueOffset, int GreenOffset)
+GameUpdateAndRender(offscreen_buffer *Buffer, s32 BlueOffset, s32 GreenOffset,
+                    sound_output_buffer *SoundBuffer, s32 ToneHz)
 {
+    GameOutputSound(SoundBuffer, ToneHz);
     RenderWeirdGradient(Buffer, BlueOffset, GreenOffset);
 }
--- handmade/handmade.h
+++ handmade/handmade.h
@@ -2,10 +2,18 @@ typedef struct offscreen_buffer
 {
     // NOTE(kstandbridge): Pixels are alwasy 32-bits wide, Memory Order BB GG RR XX
     void *Memory;
-    int Width;
-    int Height;
-    int Pitch;
+    s32 Width;
+    s32 Height;
+    s32 Pitch;
 } offscreen_buffer;
 
+typedef struct sound_output_buffer
+{
+    s32 SamplesPerSecond;
+    s32 SampleCount;
+    s16 *Samples;
+} sound_output_buffer;
+
 internal void 
-GameUpdateAndRender(offscreen_buffer *Buffer, int BlueOffset, int GreenOffset);
+GameUpdateAndRender(offscreen_buffer *Buffer, s32 BlueOffset, s32 GreenOffset,
+                    sound_output_buffer *SoundBuffer, s32 ToneHz);
